c1ccad5fdd179c47e387bee6b340c13b1c731728
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-23 18:05:47 +0530

    Update navya_reverse_lookup.py

--- navya_reverse_lookup.py
+++ navya_reverse_lookup.py
@@ -2,6 +2,4 @@ scores={"anita":92,
         "ravi":85,
         "kiran":76,
         "zoya":88}
-score=int(input("enter score:"))
-zip_data = dict(zip(scores.values(),scores.keys()))
-print(zip_data.get(score,"score not found"))
+print(dict(zip(scores.values(),scores.keys())).get(int(input("Enter score:")),"Not found"))
