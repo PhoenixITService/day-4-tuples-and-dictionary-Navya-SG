a36b36587677af9430335e7725a3334d062b3118
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-23 18:00:26 +0530

    commit

--- navya_reverse_lookup.py
+++ navya_reverse_lookup.py
@@ -2,4 +2,4 @@ scores={"anita":92,
         "ravi":85,
         "kiran":76,
         "zoya":88}
-print(dict(zip(scores.values(),scores.keys())).get(int(input("enter score:"))))
+print(dict(zip(scores.values(),scores.keys())).get(int(input("enter score:")),"score not found"))
---
+++ navya_tuple_sort.py
@@ -0,0 +1,8 @@
+data =[('A', 1, 2),
+       ('B', 3),
+       ('C', 4, 5, 6),
+       ('D',),
+       ('E', 7, 8)]
+sorted_data = sorted(data, key=len)
+extract = list(zip(*sorted_data))[0]
+print(extract)
